187098811cb9e76734dc02afaff11c48d17b79b5
macedofelipecosta <macedofelipecosta@icloud.com>
2025-06-27 22:30:03 -0300

    Ajustes de io socket en server.js para arreglar css

--- result/server.js
+++ result/server.js
@@ -5,7 +5,10 @@ var express = require('express'),
     path = require('path'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    const io = require('socket.io')(server, {
+		path: '/result/socket.io'
+	});
+
 
 // Puerto configurable por variable de entorno
 var port = process.env.PORT || 4000;
